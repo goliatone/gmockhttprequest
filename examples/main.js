c3c582e07d4dd495fede3a58784ca24d52255bd6
goliatone <burgosemi@gmail.com>
2014-12-23 13:29:33 -0500

    EXAMPLES: main, wip

--- examples/main.js
+++ examples/main.js
@@ -10,5 +10,28 @@ requirejs.config({
 
 define(['gmockhttprequest', 'jquery'], function (GMockHttpRequest, $) {
     console.log('Loading');
-	var gmockhttprequest = new GMockHttpRequest();
+    window.GMockHttpRequest = GMockHttpRequest;
+
+	var Server = new GMockHttpRequest.Server(function(){
+        console.log('server', arguments);
+    });
+    Server.start();
+    Server.respondWith('GET', 'http://localhost:9000', 'Kaka');
+
+    //=====================
+    $.ajax({url:'http://localhost:9000'})
+    .done(function(data){
+        console.log('DONE', data)
+    })
+    .fail(function(err){
+        console.log('ERROR', arguments);
+    })
+    .always(function(){
+        console.log('RUNNNNN')
+    });
+    //=====================
+
+    Server.respond();
+
+    window.Server = Server;
 });
